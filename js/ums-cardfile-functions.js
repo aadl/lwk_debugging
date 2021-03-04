5e1a3b7bbf307d257561c0ee796b642c4b5d642a
Laurie Kirchmeier <kirchmeierl@aadl.org>
2021-03-04 17:51:07 -0500

    Event Performances Reorder form added Added cardfile-lib for css and js Fixed issue with confirm dialog when deleting items - deletion of the item was occuring even when either the OK or cancel button was clicked.

--- js/ums-cardfile-functions.js
+++ js/ums-cardfile-functions.js
@@ -10,54 +10,50 @@
             var buttonName = currentElement.text();           // --- Handle Cancel pickup requests
             var buttonLink = currentElement.attr('href');
             var itemName = currentElement.data('name');  
-            var executeHREF = false;
 
             if (buttonName.startsWith('Delete'))  {
-              executeHREF = confirm('Are you sure you wish to delete the ' + typeName + ' - "' + itemName + '" ?');
-            }
-            else {
-              executeHREF = true;
-            }
-
-            if (true == executeHREF) {
+              var confirmResult = confirm('Are you sure you wish to delete the ' + typeName + ' - "' + itemName + '" ?');
+              if (confirmResult) {
                $.ajax({
-                url: buttonLink,
-                type: "GET",
-                success: function (result) {
-                  // after successful deletion from the DB, remove the row from the table
-                  if (buttonName.startsWith('Delete'))  {
-                    currentElement.parents("tr:first").remove();
-                  }           // Remove the row fron the table 
-                },
-                error: function (error) {
-                  console.log('delete ' + typeName + 'confirmation - ajax error' + JSON.stringify(error));
-                }
-              });
+                  url: buttonLink,
+                  type: "GET",
+                  success: function (result) {
+                    // after successful deletion from the DB, remove the row from the table
+                    if (buttonName.startsWith('Delete'))  {
+                      currentElement.parents("tr:first").remove();
+                    }           // Remove the row fron the table 
+                  },
+                  error: function (error) {
+                    console.log('delete ' + typeName + 'confirmation - ajax error' + JSON.stringify(error));
+                  }
+                });
+              }
+              return confirmResult;
             }
           }
                     // --------------------------------- EVENT HANDLERS ---------------------------------
           $('#ums-venues-table tbody td a').click(function (e) {
-            handleButtonClick('venue', $(this));
+            return handleButtonClick('venue', $(this));
           });
           
           $('#ums-events-table tbody td a').click(function (e) {
-            handleButtonClick('event', $(this));
+            return handleButtonClick('event', $(this));
           });        
 
           $('#ums-artists-table tbody td a').click(function (e) {
-            handleButtonClick('artist', $(this));
+            return handleButtonClick('artist', $(this));
           });        
 
           $('#ums-series-table tbody td a').click(function (e) {
-            handleButtonClick('series', $(this));
+            return handleButtonClick('series', $(this));
           });        
           
-         $('#ums-perfrole-table tbody td a').click(function (e) {
-            handleButtonClick('performance role', $(this));
+          $('#ums-perfrole-table tbody td a').click(function (e) {
+            return handleButtonClick('performance role', $(this));
           });        
  
           $('#ums-workrole-table tbody td a').click(function (e) {
-            handleButtonClick('creator role', $(this));
+            return handleButtonClick('creator role', $(this));
           });        
 
        });
